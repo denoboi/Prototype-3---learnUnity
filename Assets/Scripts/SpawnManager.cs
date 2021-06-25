365a2c4f87146c1318a96462c49809873c6aef8e
denoboi <deniz.kaymakci@hotmail.com>
2021-06-26 00:17:56 +0300

    Kamera

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    List<GameObject> prefabList = new List<GameObject>();
+    public GameObject Prefab1;
+    public GameObject Prefab2;
+    public GameObject Prefab3;
     public GameObject obstaclePrefab;
+    public GameObject obstaclePrefab2;
+    
     private Vector3 spawnPos = new Vector3(25, 0, 0); 
     private float startTime =1.5f;
     private float repeatTime = 1.5f;
@@ -13,8 +19,11 @@ public class SpawnManager : MonoBehaviour
    
     void Start()
     {
+        
+
+
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
-        InvokeRepeating("obstacleSpawn", startTime, repeatTime);
+        InvokeRepeating("ObstacleSpawn", startTime, repeatTime);
     }
 
    
@@ -22,13 +31,21 @@ public class SpawnManager : MonoBehaviour
     {
         
     }
-    void obstacleSpawn()
+    public void ObstacleSpawn()
         
     {
         if (playerControllerScript.gameOver == false)
         {
+            prefabList.Add(Prefab1);
+            prefabList.Add(Prefab2);
+            prefabList.Add(Prefab3);
+
+            int prefabIndex = Random.Range(0, 3);
+            Instantiate(prefabList[prefabIndex], spawnPos, obstaclePrefab.transform.rotation);
             Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+            Instantiate(obstaclePrefab2, spawnPos, obstaclePrefab.transform.rotation);
         }
+        
             
     }
 
